4de9a6d16abfa4201c3bb1312d23ddd3a4ed2170
Tstassin <Tstassin@users.noreply.github.com>
2023-03-11 16:02:48 +0100

    fine

--- frontend/src/pages/order.tsx
+++ frontend/src/pages/order.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Checkbox, Container, FormControl, FormErrorMessage, FormLabel, Heading, Input, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { OrderData } from '../../../backend/src/services/orders/orders.schema'
 import { client } from "../../api/api";
@@ -8,85 +8,93 @@ import { useOrderCreateMutation, useOrderDates } from "../queries/orders";
 import { useAllProducts } from "../queries/products";
 import localeData from 'dayjs/plugin/localeData'
 import fr from 'dayjs/locale/fr'
+import { QueryStatus } from "../components/queries/query-status";
 dayjs.extend(localeData)
 
 export const Order = () => {
   const orderCreateMutation = useOrderCreateMutation()
   const allProductsQuery = useAllProducts()
-  const orderDatesQuery = useOrderDates()
-  const { handleSubmit, register, clearErrors, setError, formState: { errors, isDirty } } = useForm<OrderData>();
+  const { nextWeek, nextDeliveryDates } = useOrderDates().data || {}
+  const { handleSubmit, register, watch, clearErrors, setError, formState: { errors, isDirty } } = useForm<OrderData>();
 
   const onSubmit = async (values: OrderData) => {
-    values.products.forEach(p => p.productId = parseInt(p.productId) + 1)
+    values.products.forEach(p => p.productId = parseInt(p.productId))
     orderCreateMutation.mutate(values)
   };
 
+  const nextWeekLabel = useMemo(() => {
+    return dayjs(nextWeek, 'YYYY-MM-DD').locale(fr).format('DD MMMM')
+  }, [nextWeek])
+
   return (
     <Container>
-      <Box mb={10}>
-        <Heading>Formulaire de commande</Heading>
-        <Text fontSize={'xl'}>Semaine du XXXX</Text>
-      </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <>
-          <FormControl mb={5} isInvalid={false}>
-            <FormLabel>Je viendrai chercher mon pain</FormLabel>
-            <RadioGroup >
-              <Stack >
-                {orderDatesQuery.data?.nextDeliveryDates.map(
-                  date => (
-                    <Radio
-                      value={date}
-                      key={date}
+      <QueryStatus query={allProductsQuery}>
+        <Box mb={10}>
+          <Heading>Formulaire de commande</Heading>
+          <Text fontSize={'xl'}><>Semaine du {nextWeekLabel}</></Text>
+        </Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <>
+            <FormControl mb={5} isInvalid={false}>
+              <FormLabel>Je viendrai chercher mon pain</FormLabel>
+              <RadioGroup >
+                <Stack >
+                  {nextDeliveryDates?.map(
+                    date => (
+                      <Radio
+                        value={date}
+                        key={date}
+                        {...register(
+                          "delivery",
+                          {
+                            required: "Required",
+                          }
+                        )}
+                      >
+                        {dayjs(date, 'YYYY-MM-DD').locale(fr).format('dddd DD MMMM YYYY')}
+                      </Radio>
+                    )
+                  )}
+                </Stack>
+              </RadioGroup>
+            </FormControl>
+            <FormControl mb={5} isInvalid={false}>
+              <FormLabel>Les pains que je commande</FormLabel>
+              <Stack>
+                {allProductsQuery.data?.map((product, index) =>
+                (
+                  <>
+                    <Checkbox
+                      key={product.id}
+                      value={product.id}
                       {...register(
-                        "delivery",
+                        `products.${index}.productId`
+                      )}
+                    >{product.name}, {product.price}€ pièce</Checkbox>
+                    <Input
+                      type='number'
+                      min={0}
+                      max={10}
+                      {...register(
+                        `products.${index}.amount`,
                         {
-                          required: "Required",
+                          valueAsNumber: true
                         }
                       )}
-                    >
-                      {dayjs(date, 'YYYY-MM-DD').locale(fr).format('dddd DD MMMM YYYY')}
-                    </Radio>
-                  )
+                    />
+                  </>
+                )
                 )}
               </Stack>
-            </RadioGroup>
-          </FormControl>
-          <FormControl mb={5} isInvalid={false}>
-            <FormLabel>Les pains que je commande</FormLabel>
-            <Stack>
-              {allProductsQuery.data?.map((product, index) =>
-              (
-                <>
-                  <Checkbox
-                    key={product.id}
-                    value={product.id}
-                    {...register(
-                      `products.${index}.productId`
-                    )}
-                  >{product.name}</Checkbox>
-                  <Input
-                    type='number'
-                    min={0}
-                    max={10}
-                    {...register(
-                      `products.${index}.amount`,
-                      {
-                        valueAsNumber: true
-                      }
-                    )}
-                  />
-                </>
-              )
-              )}
-            </Stack>
-          </FormControl>
+            </FormControl>
 
-          <Box>
-            <Button type="submit">Commander</Button>
-          </Box>
-        </>
-      </form>
+            <Box>
+              <Button type="submit">Commander</Button>
+            </Box>
+          </>
+        </form>
+      </QueryStatus >
     </Container>
+
   )
 }
