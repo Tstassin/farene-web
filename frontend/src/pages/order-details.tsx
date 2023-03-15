b59ac59a6f3ba816399942b49becf095de6f22f6
Tstassin <Tstassin@users.noreply.github.com>
2023-03-15 22:41:13 +0100

    PoC stripe checkout

--- frontend/src/pages/order-details.tsx
+++ frontend/src/pages/order-details.tsx
@@ -1,17 +1,36 @@
 import { Button, Container } from "@chakra-ui/react";
-import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import CheckoutForm from "../components/payments/checkout-form";
 import { QueryStatus } from "../components/queries/query-status";
 import { useOrder } from "../queries/orders";
 import { usePaymentIntentCreateMutation } from "../queries/payment-intents";
+
+const stripePromise = loadStripe("pk_test_1KaGE07z9jcdVCoJgyGByCxa");
 
 export const OrderDetailsPage = () => {
   const params = useParams()
   const orderQuery = useOrder(parseInt(params.orderId!), Boolean(params.orderId))
   const paymentIntentCreateMutation = usePaymentIntentCreateMutation()
+  const { clientSecret } = paymentIntentCreateMutation.data ?? {}
+  const appearance = {
+    theme: 'stripe' as const,
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+  console.log(clientSecret)
 
   return (
     <Container>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm clientSecret={clientSecret} />
+        </Elements>
+      )}
       <QueryStatus query={orderQuery}>
         <p>
           Numéro de commande : {orderQuery.data?.id} <br />
