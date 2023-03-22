430e383f239ee1f68037c7a17107ba11d1d8b146
Tstassin <Tstassin@users.noreply.github.com>
2023-03-22 21:43:56 +0100

    Run order + order-items creation in a transaction

--- backend/src/services/orders/orders.hooks.ts
+++ backend/src/services/orders/orders.hooks.ts
@@ -1,33 +1,33 @@
 import { BadRequest, FeathersError } from "@feathersjs/errors/lib";
 import { HookContext } from "@feathersjs/feathers";
 import dayjs from "dayjs";
-import { allowedWeekDays } from "../../config/orders";
 import { Application, NextFunction } from "../../declarations";
-import { OrderData, OrderParams, OrderService } from "./orders.class";
-import weekday from "dayjs/plugin/weekday";
-import isoWeek from "dayjs/plugin/isoWeek";
+import { Order, OrderData, OrderParams, OrderService } from "./orders.class";
+import { transaction } from "@feathersjs/knex/lib";
 
 export const createOrderItems = async (
   context: HookContext<Application, OrderService<OrderParams>>,
   next: NextFunction
 ) => {
   const data = context.data as OrderData
-  if (!data || !("orderItems" in data) || !Array.isArray(data.orderItems) || data.orderItems.length === 0) {
+  if (data.orderItems.length === 0) {
     throw new BadRequest("No orderItems in order");
   }
   const { orderItems } = data;
-  await next();
-  if (
-    context.result === undefined ||
-    Array.isArray(context.result) ||
-    "total" in context.result
-  ) {
-    throw new BadRequest("Hmmm shouldn't happen");
+  try {
+    transaction.start()
+    await next();
+    const result = context.result as Order
+    const { id: orderId } = result;
+    await context.app
+      .service("order-items")
+      .create(orderItems.map((orderItem) => ({ ...orderItem, orderId })));
+    transaction.end()
   }
-  const { id: orderId } = context.result;
-  await context.app
-    .service("order-items")
-    .create(orderItems.map((orderItem) => ({ ...orderItem, orderId })));
+  catch (err) {
+    transaction.rollback()
+    throw err
+  }
 };
 
 export const checkDeliveryDate = async (
