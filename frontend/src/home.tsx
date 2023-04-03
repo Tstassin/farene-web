02be6cda0000e76487221769ee8fdd5d456d20fb
Tstassin <Tstassin@users.noreply.github.com>
2023-04-03 16:00:37 +0200

    Export order and delivery instructions as component for easy reuse

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,9 +1,10 @@
-import { Heading, Text, Link } from "@chakra-ui/react";
+import { Heading, Text, Link, Box } from "@chakra-ui/react";
 import React from "react";
 import { NavLink, Outlet, useLoaderData } from "react-router-dom";
 import { User } from "../../backend/src/services/users/users.schema";
 import { Authenticated, NotAuthenticated } from "./components/auth/authenticated";
 import { Layout } from "./components/layout";
+import { OrderInstructions } from "./components/orders/order-instructions";
 
 export const Home: React.FC = () => {
   const user = useLoaderData() as User | null
@@ -14,20 +15,23 @@ export const Home: React.FC = () => {
   return (
     <>
       <Heading mb={5}>Bienvenue dans l'eshop de Farène</Heading>
-      <Authenticated>
-        <Text fontSize={'xl'}>
-          <Link as={NavLink} to='/order'><u>Commandez</u></Link> du pain pour la semaine prochaine.
-        </Text>
-      </Authenticated>
-      <NotAuthenticated>
-        <>
+      <Box mb={10}>
+        <Authenticated>
           <Text fontSize={'xl'}>
-            <Link as={NavLink} to='/register'><u>Créez un compte</u></Link> pour commander et payer en ligne.
-            <br />
-            <Link as={NavLink} to='/login'><u>Connectez-vous</u></Link> directement.
+            <Link as={NavLink} to='/order'><u>Commandez</u></Link> du pain pour la semaine prochaine.
+          </Text>
+        </Authenticated>
+        <NotAuthenticated>
+          <>
+            <Text fontSize={'xl'}>
+              <Link as={NavLink} to='/register'><u>Créez un compte</u></Link> pour commander et payer en ligne.
+              <br />
+              <Link as={NavLink} to='/login'><u>Connectez-vous</u></Link> directement.
             </Text>
-        </>
-      </NotAuthenticated>
+          </>
+        </NotAuthenticated>
+      </Box>
+      <OrderInstructions />
     </>
   )
 }
