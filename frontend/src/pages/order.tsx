0d9102bcceb16bdf7148e6a331c8dd6b33d23a7a
Tstassin <Tstassin@users.noreply.github.com>
2023-04-03 15:36:23 +0200

    Add order and delivery instructions

--- frontend/src/pages/order.tsx
+++ frontend/src/pages/order.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Divider, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Divider, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, ListItem, Radio, RadioGroup, Stack, Text, UnorderedList } from "@chakra-ui/react";
 import { useMemo } from "react";
 import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 import { OrderData } from '../../../backend/src/services/orders/orders.schema'
@@ -8,7 +8,7 @@ import { useAllProducts } from "../queries/products";
 import localeData from 'dayjs/plugin/localeData'
 import fr from 'dayjs/locale/fr'
 import { QueryStatus } from "../components/queries/query-status";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ProductInput } from "../components/products/product-input";
 dayjs.extend(localeData)
 
@@ -52,6 +52,9 @@ export const Order = () => {
         <Box mb={10}>
           <Heading>Formulaire de commande</Heading>
           <Text fontSize={'xl'}><>Pour la semaine du {nextWeekLabel}</></Text>
+          <Text fontSize='sm'>
+            Commandes jusque dimanche minuit
+          </Text>
         </Box>
         <FormProvider {...methods}>
           <form onSubmit={handleSubmit(onSubmit)}>
@@ -80,8 +83,27 @@ export const Order = () => {
                     )}
                   </Stack>
                 </RadioGroup>
-                <FormHelperText>Choisissez le jour où vous viendrez chercher votre commande</FormHelperText>
-
+                <FormHelperText>
+                  <Box mt={5}>
+                    <UnorderedList>
+                      <ListItem mb={3}>
+                        de <b>11h à 13h chez Farène</b> : <br />
+                        <Text>
+                          Gare de Chastre, quai n°2<br />
+                          Rue Gaston Delvaux, 1A<br />
+                          1450 Chastre
+                        </Text>
+                      </ListItem>
+                      <ListItem mb={3}>
+                        de <b>14h à 21h en point dépot libre accès</b> :<br />
+                        <Text>
+                          Rue des Trois Ruisseaux, 9<br />
+                          1450 Chastre
+                        </Text>
+                      </ListItem>
+                    </UnorderedList>
+                  </Box>
+                </FormHelperText>
                 <FormErrorMessage>{errors.delivery?.message?.toString()}</FormErrorMessage>
               </FormControl>
               <FormControl isRequired mt={10} isInvalid={Boolean(errors.orderItems?.root?.type === 'required')}>
