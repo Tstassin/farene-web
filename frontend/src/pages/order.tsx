3df8eeb2c14eed3d5e9d5395c10acc49f103bd17
Tstassin <Tstassin@users.noreply.github.com>
2023-04-03 19:47:49 +0200

    parseInt not more needed

--- frontend/src/pages/order.tsx
+++ frontend/src/pages/order.tsx
@@ -35,9 +35,8 @@ export const Order = () => {
       .reduce((acc, curr) => acc + curr.amount * curr.price, 0)
     : 0
 
-
   const onSubmit = async (values: OrderData) => {
-    values.orderItems.forEach(orderItem => orderItem.productId = parseInt(orderItem.productId))
+    console.log(values)
     orderCreateMutation.mutate(values)
   };
 
