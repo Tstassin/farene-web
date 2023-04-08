f632809374308eb264b8a0e2b3a2164ed109b8a2
Tstassin <Tstassin@users.noreply.github.com>
2023-04-08 22:42:58 +0200

    frontend api hostname env var

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,6 +1,8 @@
 import { createClient } from '../../backend/src/client'
 import rest from '@feathersjs/rest-client'
 
+export const apiHostname = process.env.API_HOSTNAME || 'http://localhost:3030'
+
 const fetch = window.fetch.bind(window)
-const connection = rest('http://localhost:3030').fetch(fetch)
+const connection = rest(apiHostname).fetch(fetch)
 export const client = createClient(connection)
