2ef6aa75e6f316e5dff880095588f8df8935c0c2
Tstassin <Tstassin@users.noreply.github.com>
2023-04-27 21:44:41 +0200

    Wider screen for admins on desktop

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,12 +1,14 @@
-import { Container } from "@chakra-ui/react";
-import React, { PropsWithChildren } from "react";
-import { Outlet } from "react-router-dom";
+import { Container, useMediaQuery } from "@chakra-ui/react";
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./header";
 
 export const Layout: React.FC = () => {
-
+  const location = useLocation()
+  const [isDisplayingInBrowser] = useMediaQuery(['(display-mode: browser)'])
+  const isAdminPath = location.pathname.includes('/admin')
   return (
-    <Container mb={20}>
+    <Container mb={20} maxW={isDisplayingInBrowser && isAdminPath ? '120ch' : '60ch'}>
       <Header />
       <Outlet />
     </Container>
